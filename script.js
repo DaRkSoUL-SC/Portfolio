f9efeb651b0c79a9febfbc31e7e2ef3777aab6e7
DaRkSoUL-SC <101863833+DaRkSoUL-SC@users.noreply.github.com>
2023-01-31 16:44:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -17,3 +17,22 @@ document.querySelectorAll('.nav-links').forEach(n => n.addEventListener("click",
 setTimeout(function(){ 
     document.getElementById('preloader').style.display = 'none';
 }, 2000); // it will remove after 2 seconds
+
+// Scroll Animation
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
